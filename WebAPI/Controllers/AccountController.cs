6c4447cec08915133bb221958415d5f017f3ef6a
Qamar Hafeezi <qamar@aciano.net>
2021-05-17 22:15:01 +0500

    created Login endpoint

--- WebAPI/Controllers/AccountController.cs
+++ WebAPI/Controllers/AccountController.cs
@@ -12,15 +12,15 @@ namespace WebAPI.Controllers
     public class AccountController : BaseApiController
     {
         public DataContext _context { get; }
-        public AccountController(DataContext context )
+        public AccountController(DataContext context)
         {
             _context = context;
         }
 
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register( RegisterDto registerDto)
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
         {
-            if(await UserExists(registerDto.UserName)) return BadRequest ("User Name is not available");
+            if (await UserExists(registerDto.UserName)) return BadRequest("User Name is not available");
 
             using var hmc = new HMACSHA512();
             var user = new AppUser()
@@ -36,6 +36,28 @@ namespace WebAPI.Controllers
 
         }
 
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync
+            (x => x.UserName == loginDto.UserName.ToLower());
+            if (user == null)
+            {
+                return Unauthorized("User not found");
+            }
+            using var hmc = new HMACSHA512(user.PasswordSalt);
+            var hash = hmc.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i < hash.Length; i++)
+            {
+                if (hash[i] != user.PasswordHash[i])
+                {
+                    return Unauthorized("Invalid Password");
+                }
+            }
+
+            return user;
+        }
         private async Task<bool> UserExists(string userName)
         {
             return await _context.Users.AnyAsync(
--- WebAPI/DTOs/LoginDto.cs
+++ WebAPI/DTOs/LoginDto.cs
@@ -2,11 +2,11 @@ using System.ComponentModel.DataAnnotations;
 
 namespace WebAPI.DTOs
 {
-    public class RegisterDto
+    public class LoginDto
     {
         [Required]
         public string UserName { get; set; }
-        
+
         [Required]
         public string Password { get; set; }
     }
